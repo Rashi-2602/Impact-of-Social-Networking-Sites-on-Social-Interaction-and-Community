ea1651b24dcdf6cf9a12acc1dd7f1d49fbe2b640
Rashi Pant <83759812+Rashi-2602@users.noreply.github.com>
2022-04-11 01:07:06 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,84 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How much time do you spend on social media daily (on an average)?",
+    answer: "1-2 hours",
+    options: [
+      "1-2 hours",
+      "2-3 hours",
+      "3-5 hours",
+      ">5 hours"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Why do you use an online social network?",
+    answer: "To find information",
+    options: [
+      "To share videos/pictures/music",
+      "To make new friends",
+      "To find information",
+      "To get opinions"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many social networking accounts do you maintain?",
+    answer: "4-6",
+    options: [
+      "4-6",
+      "6-10",
+      "10-15",
+      ">15"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How familiar are you with social networking sites?",
+    answer: "Expert",
+    options: [
+      "Comfortable",
+      "Expert",
+      "Novice",
+      "Not familiar at all"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Do you accept friend/follow requests from strangers on social networking sites?",
+    answer: "No",
+    options: [
+      "Yes",
+      "Sometimes",
+      "No",
+      "You have public accounts"
+    ]
+  },
+  
+
+  {
+  numb: 6,
+  question: "For how long have you been using social networking sites?",
+  answer: "1-3 years",
+  options: [
+     ">7 years",
+     "5-7 years",
+     "3-5 years",
+     "1-3 years"
+     ]
+   },
+
+
+   {
+    numb: 7,
+    question: "Mention your overall opinion about the general impact of social networking sites on social interaction & community.",
+    answer: "Both",
+    options: [
+       "Positive impact",
+       "Negative impact",
+       "Both",
+       "No opinion"
+       ]
+     },
+];
